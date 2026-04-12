a62effd441386be01713b7aed36a3a39896c1320
theUGG0 <joonatanjj@gmail.com>
2026-04-12 15:57:27 +0300

    Add examples code for parsin csv files and testing the library

---
+++ examples/main.cpp
@@ -0,0 +1,30 @@
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <vector>
+#include "../cardio.hpp"
+
+using namespace std;
+
+vector<float> read_csv_data(string filename){
+    regex re{","};
+    ifstream file{filename};
+    vector<float> data{};
+    string line{};
+    
+    //skip header
+    getline(file, line);
+    
+    while (getline(file, line)) {
+        vector tokens(sregex_token_iterator(line.begin(),line.end(), re, -1), {});
+        data.push_back(stof(tokens[2]));
+    }
+    return data;
+}
+
+int main(){
+    vector<float> data{read_csv_data("100_ekg.csv")};
+    cout << data[67];
+}
