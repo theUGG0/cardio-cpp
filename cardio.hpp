cd47d1198c86cb7c13bfe9977f2eb48e0395e94d
theUGG0 <joonatanjj@gmail.com>
2026-04-20 23:52:30 +0300

    Add pan-tompkins initial implementation

--- cardio.hpp
+++ cardio.hpp
@@ -1,6 +1,10 @@
+#include <algorithm>
 #include <cmath>
 #include <cstddef>
 #include <cstdint>
+#include <fstream>
+#include <numeric>
+#include <unordered_set>
 #include <vector>
 
 struct HeartBeat
@@ -21,10 +25,9 @@ struct HRVStatus
 
 class HeartRateAnalyzer
 {
-    std::vector<HeartBeat> _pam_tompkins(std::vector<float>& raw_data_mv, unsigned int sampling_rate_hz);
-
     public:
         HeartRateAnalyzer() = default;
+        std::vector<HeartBeat> _pam_tompkins(std::vector<float>& raw_data_mv, unsigned int sampling_rate_hz);
 
         int add_data(std::vector<float> data);
         int push_data(float sample, uint32_t timestamp_ms);
@@ -75,7 +78,7 @@ std::vector<HeartBeat> HeartRateAnalyzer::_pam_tompkins(std::vector<float>& raw_
     std::vector<float> integrated(squared.size(), 0.0f);
 
     float sliding_sum{};
-    for (int i = 0; i < window_size; i++) {
+    for (unsigned int i = 0; i < window_size; i++) {
         sliding_sum += squared[i];
     }
     integrated[window_size - 1] = sliding_sum / window_size;
@@ -83,5 +86,53 @@ std::vector<HeartBeat> HeartRateAnalyzer::_pam_tompkins(std::vector<float>& raw_
     for (size_t i = window_size; i < squared.size(); i++){
         sliding_sum = sliding_sum - squared[i-window_size] + squared[i];
         integrated[i] = sliding_sum / window_size;
-    }   
+    }
+
+    std::vector<size_t> R_peaks{};
+
+    double sig = 0.5 * (*std::max_element(integrated.begin(), integrated.end()));
+    double noise = std::accumulate(integrated.begin(), integrated.end(), 0.0f) / integrated.size();
+    double thresh = noise + 0.25 * (sig - noise);
+    size_t last_peak_index = 0;
+    
+    const size_t refractory_period_len = sampling_rate_hz * 0.2;
+
+    for (size_t i = 2; i < integrated.size(); i++){ // implement cross referencing with highpass filtered value
+        if (integrated[i-2] < integrated[i-1] && integrated[i-1] > integrated[i]){
+            float peak_value = integrated[i-1];
+
+            bool refractory_period_inactive = (last_peak_index == 0) || (i-1 - last_peak_index > refractory_period_len);
+
+            if (peak_value > thresh && refractory_period_inactive){
+                sig = 0.125 * peak_value + 0.875 * sig;
+                R_peaks.push_back(i-1);
+                last_peak_index = i-1;
+            }
+            else {
+                noise = 0.125 * peak_value + 0.875 * noise;
+            }
+            thresh = noise + 0.25 * (sig - noise);
+        }
+    }
+
+    std::ofstream file{"testout.csv"};
+    file << "index,integrated,r_peak_marker\n";
+
+    std::unordered_set<size_t> r_peak_set(R_peaks.begin(), R_peaks.end());
+
+    for (size_t i = 0; i < integrated.size(); i++){
+        file << i << "," << integrated[i] << ",";
+        
+        // Put the integrated value at R-peaks, empty otherwise
+        if (r_peak_set.count(i) > 0){
+            file << integrated[i];  // Same value as integrated
+        }
+        // else: leave empty
+        
+        file << "\n";
+    }
+
+    file.close();
+
+    return {};
 }
